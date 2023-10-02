2bee6bd25a0e8ce803e4008aba8ec95d09128a41
Kaleb Perez <kap222@txstate.edu>
2023-10-02 10:24:15 -0500

    added + split  button
    no funtionality yet

--- Hercules/Hercules/Views/workout.js
+++ Hercules/Hercules/Views/workout.js
@@ -1,14 +1,39 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { ScrollView, View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 const WorkoutView = () => {
-  return <View style={styles.container}></View>;
+  return (
+    <ScrollView contentContainerStyle={styles.container} style={styles.scrollView}>
+      <TouchableOpacity style={styles.addButton}>
+        <Text style={styles.buttonText}>+</Text>
+      </TouchableOpacity>
+    </ScrollView>
+  );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  scrollView: {
     flex: 1,
     backgroundColor: 'black',
+  },
+  container: {
+    flexGrow: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    paddingTop: 100,
+  },
+  addButton: {
+    backgroundColor: 'purple',
+    width: 100,
+    height: 50,
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 24,
   }
 });
 
