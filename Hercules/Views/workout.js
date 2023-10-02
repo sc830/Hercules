6b30cc91464fa305d65373eb0a36e32f9813567f
Kaleb Perez <kap222@txstate.edu>
2023-10-02 12:51:05 -0500

    Added drop down menu to add split name

--- Hercules/Views/workout.js
+++ Hercules/Views/workout.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import { ScrollView, View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { ScrollView, View, Text, StyleSheet, TouchableOpacity, Modal, TextInput } from 'react-native';
 
 const WorkoutView = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [splitName, setSplitName] = useState('');
+
   return (
     <ScrollView contentContainerStyle={styles.container} style={styles.scrollView}>
-      <TouchableOpacity style={styles.addButton}>
+      <TouchableOpacity style={styles.addButton} onPress={() => setShowModal(true)}>
         <Text style={styles.buttonText}>+</Text>
       </TouchableOpacity>
+      <Modal animationType="slide" transparent={true} visible={showModal}>
+        <View style={styles.modalView}>
+          <TextInput
+            style={styles.input}
+            value={splitName}
+            onChangeText={setSplitName}
+            placeholder="Enter Split Name"
+          />
+          <TouchableOpacity style={styles.addButtonModal} onPress={() => setShowModal(false)}>
+            <Text style={styles.buttonText}>Confirm</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
     </ScrollView>
   );
 };
@@ -34,6 +50,28 @@ const styles = StyleSheet.create({
   buttonText: {
     color: 'white',
     fontSize: 24,
+  },
+  modalView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Semi-transparent background
+  },
+  input: {
+    width: '80%',
+    height: 50,
+    backgroundColor: 'white',
+    marginBottom: 10,
+    paddingHorizontal: 10,
+    fontSize: 20,
+  },
+  addButtonModal: {  // Added this to differentiate between the '+' button and the modal button
+    backgroundColor: 'purple',
+    width: '80%',
+    height: 50,
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
   }
 });
 
