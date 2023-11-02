0f4f97dbed765a332ff8d53e5e9a61fb5b7cff6d
Shelby Cumings <s.rs.cumings@gmail.com>
2023-11-02 18:51:08 -0500

    Changed addDoc to not cause error

--- Hercules/App.js
+++ Hercules/App.js
@@ -59,8 +59,9 @@ const MunchiesScreen = () => (
 );
 
 const App = () => {
-  /* try {
-    const docRef = await addDoc(collection(db, "users"), {   // adds docs to collection userData
+  /*
+  try {
+    const docRef = addDoc(collection(db, "users"), {   // adds docs to collection userData
       first: "Ada",                                             // if it doesn't exist, creates collection userData
       last: "Lovelace",                                         // creates new collection with three documents:
       born: 1815                                                // first, last, and born
@@ -68,7 +69,8 @@ const App = () => {
     console.log("Document written with ID: ", docRef.id);
   } catch (e) {
     console.error("Error adding document: ", e);
-  } */
+  }
+  */
   return (
     <NavigationContainer>
       <Tab.Navigator
