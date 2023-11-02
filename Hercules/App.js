4e90566067d2b7cd89aceb5f6243e4543ee27ce6
Shelby Cumings <s.rs.cumings@gmail.com>
2023-11-02 17:21:00 -0500

    Firebase config added, Firebase "add doc" test functional

--- Hercules/App.js
+++ Hercules/App.js
@@ -14,6 +14,19 @@ import BreakfastScreen from './Views/BreakfastScreen';
 import LunchScreen from './Views/LunchScreen';
 import DinnerScreen from './Views/DinnerScreen';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCoxkZvQiNLd-wZrqSJB8dKE2EyHJ11O8U",
+  authDomain: "hercules-fc42e.firebaseapp.com",
+  projectId: "hercules-fc42e",
+  storageBucket: "hercules-fc42e.appspot.com",
+  messagingSenderId: "501456721475",
+  appId: "1:501456721475:web:7cc30d4a6c8f34320826f6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
