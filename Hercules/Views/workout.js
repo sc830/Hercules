40e88cbcda2cba7afdb30c05c540efe8c19cbc0f
Kacy <yot5@txstate.edu>
2023-11-03 00:50:53 -0500

    index is declared but never referenced. I give up

--- Hercules/Views/workout.js
+++ Hercules/Views/workout.js
@@ -19,24 +19,41 @@ const WorkoutView = () => {
   const [renameIndex, setRenameIndex] = useState(-1);
   const [showDeleteOption, setShowDeleteOption] = useState(-1); 
 
+  
   const renderItem = ({ item, index, drag, isActive }) => {
     return (
       <TouchableOpacity
-        style={{
-          height: 100,
-          backgroundColor: isActive ? 'blue' : 'grey',
-          alignItems: 'center',
-          justifyContent: 'center'
-        }}
-        onLongPress={drag}
+        onLongPress={drag} // Assign the drag function to the onLongPress event
+        disabled={isActive}
+        style={[
+          styles.splitButton,
+          isActive ? { backgroundColor: 'blue' } : {}, // Optional: change the style if the item is active (being dragged)
+        ]}
       >
-        <Text style={{ fontWeight: 'bold', color: 'white' }}>{item}</Text>
+        <Text style={styles.splitText}>{item}</Text>
+
+        <TouchableOpacity 
+          style={styles.settingsButton} 
+          onPress={() => {
+            setShowDeleteOption(index === showDeleteOption ? -1 : index);
+            setRenameIndex(index === renameIndex ? -1 : index);
+          }}
+        >
+          <Text style={styles.settingsText}>⚙️</Text>
+        </TouchableOpacity>
+  
+        {showDeleteOption === index && (
+          <View style={styles.buttonRow}>
+            <TouchableOpacity onPress={() => deleteSplit(index)} style={styles.actionButton}>
+              <Text style={styles.actionButtonText}>Delete</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => handleRenameOpen(index, item)} style={styles.actionButton}>
+              <Text style={styles.actionButtonText}>Rename</Text>
+            </TouchableOpacity>
+          </View>
+        )}
       </TouchableOpacity>
     );
-  };
-
-  const drag = () => {
-    // Define drag functionality or leave it to the DraggableFlatList to handle.
   };
 
   const addSplit = () => {
@@ -92,44 +109,7 @@ const WorkoutView = () => {
         onDragEnd={({ data }) => setSplits(data)}
       />
 
-      {/* {splits.map((split, index) => (
-        <View key={index} style={styles.splitContainer}>
-          <DraggableFlatList
-            data={splits}
-            renderItem={renderItem}
-            keyExtractor={(item, index) => `draggable-item-${index}`}
-            onDragEnd={({ data }) => setSplits(data)}
-          />
-          <TouchableOpacity
-            style={styles.splitButton}
-            onPress={() => navigation.navigate('workoutList', { splitName: split })}
-            onLongPress={drag}
-          >
-            <Text style={styles.splitText}>{split}</Text>
-            <TouchableOpacity 
-              style={styles.settingsButton} 
-              onPress={() => {
-                setShowDeleteOption(index === showDeleteOption ? -1 : index);
-                setRenameIndex(index === renameIndex ? -1 : index);
-              }}
-            >
-              <Text style={styles.settingsText}>⚙️</Text>
-            </TouchableOpacity>
-          </TouchableOpacity>
-
-          {showDeleteOption === index && (
-            <View style={styles.buttonRow}>
-             <TouchableOpacity onPress={() => deleteSplit(index)} style={styles.actionButton}>
-                <Text style={styles.actionButtonText}>Delete</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => handleRenameOpen(index, split)} style={styles.actionButton}>
-                <Text style={styles.actionButtonText}>Rename</Text>
-            </TouchableOpacity>
-        </View>
-        
-          )}
-        </View>
-      ))} */}
+      
 
       <TouchableOpacity style={styles.addButton} onPress={() => setShowModal(true)}>
         <Text style={styles.buttonText}>+ Add Workout Day</Text>
