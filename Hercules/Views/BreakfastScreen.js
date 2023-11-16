589301c24a9be0457a736a7e22d58054999b334e
Aaron Parks <amp421@txstate.edu>
2023-11-15 20:26:05 -0600

    Finished the API implementation

--- Hercules/Views/BreakfastScreen.js
+++ Hercules/Views/BreakfastScreen.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, ScrollView, TextInput } from 'react-native';
-import searchMeal from './API'; // Adjust the import path as necessary
+import { View, Text, TouchableOpacity, ScrollView, TextInput, Button, StyleSheet } from 'react-native';
+import searchMeal from './API'; // Make sure this path is correct
 
 const BreakfastScreen = ({ navigation }) => {
   const [mealName, setMealName] = useState('');
@@ -21,10 +21,13 @@ const BreakfastScreen = ({ navigation }) => {
     } else {
       try {
         const fetchedMealData = await searchMeal(mealItem.name);
+        // Assuming fetchedMealData.hints contains your meals
+        // Modify this line to match the structure of your actual API response
+        const topFiveMeals = fetchedMealData.hints.slice(0, 5);
         const newMealList = [...mealList];
-        newMealList[index].data = fetchedMealData;
+        newMealList[index].data = { ...fetchedMealData, hints: topFiveMeals };
         setMealList(newMealList);
-        navigation.navigate('IngredientsScreen', { mealData: fetchedMealData });
+        navigation.navigate('IngredientsScreen', { mealData: newMealList[index].data });
       } catch (error) {
         alert('Failed to fetch meal data.');
         console.error('Error fetching meal info:', error);
@@ -33,26 +36,67 @@ const BreakfastScreen = ({ navigation }) => {
   };
 
   return (
-    <View style={{ flex: 1, backgroundColor: 'black' }}>
+    <View style={styles.container}>
       <TextInput
         value={mealName}
         onChangeText={setMealName}
         placeholder="Enter meal name"
         placeholderTextColor="gray"
-        style={{ color: 'white', borderColor: 'gray', borderWidth: 1, borderRadius: 5, padding: 10, margin: 20, width: '80%' }}
+        style={styles.input}
       />
-      <TouchableOpacity onPress={handleAddMeal} style={{ backgroundColor: 'purple', padding: 20, margin: 20, width: '80%' }}>
-        <Text style={{ color: 'white', textAlign: 'center' }}>Add Meal</Text>
+      <TouchableOpacity onPress={handleAddMeal} style={styles.addButton}>
+        <Text style={styles.addButtonText}>Add Meal</Text>
       </TouchableOpacity>
       <ScrollView>
         {mealList.map((mealItem, index) => (
-          <TouchableOpacity key={index} style={{ backgroundColor: 'lightgrey', padding: 10, margin: 5, borderRadius: 5 }} onPress={() => handleMealPress(mealItem, index)}>
-            <Text style={{ color: 'black' }}>{mealItem.name}</Text>
+          <TouchableOpacity key={index} onPress={() => handleMealPress(mealItem, index)} style={styles.mealItem}>
+            <Text style={styles.mealItemText}>{mealItem.name}</Text>
           </TouchableOpacity>
         ))}
       </ScrollView>
+      <Button 
+        title="Back" 
+        onPress={() => navigation.goBack()} 
+        color="purple" 
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+  },
+  input: {
+    color: 'white',
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    margin: 20,
+    width: '80%',
+  },
+  addButton: {
+    backgroundColor: 'purple',
+    padding: 20,
+    margin: 20,
+    width: '80%',
+  },
+  addButtonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  mealItem: {
+    backgroundColor: 'lightgrey',
+    padding: 10,
+    margin: 5,
+    borderRadius: 5,
+  },
+  mealItemText: {
+    color: 'black',
+  },
+  // Add any additional styles you need here
+});
 
 export default BreakfastScreen;
