9247fdd54d5fb5e2082f9b7f6c1157869936d68e
Kacy <yot5@txstate.edu>
2023-11-17 14:28:19 -0600

    updated colors

--- Hercules/Views/mindFolder/Mindfulness.js
+++ Hercules/Views/mindFolder/Mindfulness.js
@@ -16,95 +16,112 @@ const Mindfulness = ({ navigation }) => {
     submitCustomTracker,
   } = useCustomTracker(['Creatine', 'Sleep hours', 'Water Ounces']);
 
-    const handleAddTrackerButtonPress = () => {
-      handleAddCustomTracker();
+  const renderTrackers = () => {
+    const trackerData = {
+      'Water Ounces': [64, 72, 68, 80, 76],
+      'Sleep hours': [7, 8, 6, 5, 9],
+      'Creatine': [5, 5, 5, 5, 5]
     };
+  
+    const labels = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'];
+  
+    return trackers.map((tracker, index) => {
+      if (tracker === 'Add a Custom Tracker') {
+        return null;
+      }
+  
+      const data = trackerData[tracker];
+  
+      return (
+        <View key={index} style={styles.graphContainer}>
+          <Text style={styles.graphLabel}>{tracker}</Text>
+          <TouchableOpacity onPress={() => navigation.navigate('TrackIntakeScreen', { itemType: tracker })}>
+            <GraphWithButton
+              data={data}
+              labels={labels}
+              buttonText={tracker}
+              onButtonPress={() => console.log('Navigate to the next page')}
+            />
+          </TouchableOpacity>
+        </View>
+      );
+    });
+  };
 
-    const renderTrackers = () => {
-      const trackerData = {
-        'Water Ounces': [64, 72, 68, 80, 76],
-        'Sleep hours': [7, 8, 6, 5, 9],
-        'Creatine': [5, 5, 5, 5, 5]
-      };
-    
-      const labels = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'];
-    
-      return trackers.map((tracker, index) => {
-        if (tracker === 'Add a Custom Tracker') {
-          return null;
-        }
-    
-        const data = trackerData[tracker];
-    
-        return (
-          <View key={index} style={styles.graphContainer}>
-            <Text style={styles.graphLabel}>{tracker}</Text>
-            <TouchableOpacity onPress={() => navigation.navigate('TrackIntakeScreen', { itemType: tracker })}>
-              <GraphWithButton
-                data={data}
-                labels={labels}
-                buttonText={tracker}
-                onButtonPress={() => console.log('Navigate to the next page')}
-              />
-            </TouchableOpacity>
-          </View>
-        );
-      });
-    };
+  const handleAddTrackerButtonPress = () => {
+    handleAddCustomTracker();
+  };
 
-return (
-  <View style={{flex: 1, backgroundColor: 'black'}}>
-    <ScrollView style={styles.container}>
-      {renderTrackers()}
-      <TouchableOpacity style={styles.addButton} onPress={handleAddTrackerButtonPress}>
-        <Text style={styles.buttonText}>Add Custom Tracker</Text>
-      </TouchableOpacity>
-    </ScrollView>
-    <CustomTrackerModal 
-      modalVisible={modalVisible}
-      setModalVisible={setModalVisible}
-      customTrackerName={customTrackerName}
-      setCustomTrackerName={setCustomTrackerName}
-      submitCustomTracker={() => {
-        submitCustomTracker();
-        // Optional: Add logic here to update graphs or data
-      }}
-    />
-  </View>
-);
+  const onCustomTrackerSubmit = () => {
+    submitCustomTracker();
+    // Make sure the graphs are updated here if necessary.
+    // You might need to set some state that triggers the graphs to re-render.
+  };
+
+  return (
+    <View style={{flex: 1, backgroundColor: 'black'}}>
+      <ScrollView style={styles.container}>
+        {renderTrackers()}
+        <TouchableOpacity style={styles.addButton} onPress={handleAddTrackerButtonPress}>
+          <Text style={styles.buttonText}>Add Custom Tracker</Text>
+        </TouchableOpacity>
+      </ScrollView>
+      <CustomTrackerModal 
+        modalVisible={modalVisible}
+        setModalVisible={setModalVisible}
+        customTrackerName={customTrackerName}
+        setCustomTrackerName={setCustomTrackerName}
+        submitCustomTracker={onCustomTrackerSubmit}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
-    backgroundColor: 'black'
+    backgroundColor: '#FFF7E0',
   },
   graphContainer: {
-    width: '90%',            // Adjust the width as needed
-    alignItems: 'center',    // Center content of each graph container
-    marginBottom: 20         // Add space between each graph
+    width: '90%',
+    alignItems: 'center',
+    marginBottom: 20,
+    alignSelf: 'center',
   },
   graphLabel: {
     color: 'white',
     fontSize: 16,
     textAlign: 'center',
-    marginBottom: 5 // Space above the graph
+    marginBottom: 5,
   },
+  addButton: {
+    backgroundColor: '#D4AF37',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 25,
+    marginVertical: 10,
+    alignSelf: 'center',
+    width: '80%',
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    fontSize: 18,
+    elevation: 2,
+  },
+
   innerContainer: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
   },
   button: {
-    backgroundColor: 'purple',
+    backgroundColor: '#D4AF37',
     padding: 20,
     margin: 10,
     borderRadius: 15,
     width: '90%'
-  },
-  buttonText: {
-    color: 'white',
-    textAlign: 'center'
   },
   centeredView: {
     flex: 1,
