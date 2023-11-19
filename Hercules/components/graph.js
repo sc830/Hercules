9c790325015cdc74d234fe21b1b8c8f3beeef140
Kacy <yot5@txstate.edu>
2023-11-19 11:15:20 -0600

    fixed data not updating bug

--- Hercules/components/graph.js
+++ Hercules/components/graph.js
@@ -1,72 +1,83 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 import { LineChart } from 'react-native-chart-kit';
-import moment from 'moment'; // Make sure to install moment via npm or yarn
+import moment from 'moment';
 
 const GraphWithButton = ({ initialData, labels, onButtonPress, trackerTitle }) => {
-    const defaultData = new Array(labels.length).fill(0);
-    const [data, setData] = useState(defaultData);
-  
-    // Using useEffect to set data when initialData changes
-    useEffect(() => {
-      if (initialData && Array.isArray(initialData) && initialData.length === labels.length) {
-        setData(initialData);
-      } else {
-        setData(defaultData); // Fallback to default data in case of error
-      }
-    }, [initialData]);
-    
-    const [currentWeekStart, setCurrentWeekStart] = useState(moment().startOf('week'));
+  const [dataByWeek, setDataByWeek] = useState({});
+  const [currentWeekStart, setCurrentWeekStart] = useState(moment().startOf('week').format('YYYY-MM-DD'));
 
-    const formatDateRange = (start) => {
-        const end = start.clone().endOf('week');
-        return `${start.format('MMM DD, YYYY')} - ${end.format('MMM DD, YYYY')}`;
-    };
+  // This effect sets the initial data for the current week when the component mounts.
+  useEffect(() => {
+    setDataByWeek(prevData => ({
+      ...prevData,
+      [currentWeekStart]: initialData && Array.isArray(initialData) && initialData.length === labels.length
+        ? initialData
+        : new Array(labels.length).fill(0),
+    }));
+  }, [initialData, labels.length]); // Removed currentWeekStart from the dependency array
 
-    const handlePreviousWeek = () => {
-        setCurrentWeekStart(currentWeekStart.clone().subtract(1, 'week'));
-        // You'll also want to load the data for the new week here
-    };
+  const formatDateRange = (start) => {
+    const end = moment(start).endOf('week');
+    return `${moment(start).format('MMM DD, YYYY')} - ${end.format('MMM DD, YYYY')}`;
+  };
 
-    const handleNextWeek = () => {
-        setCurrentWeekStart(currentWeekStart.clone().add(1, 'week'));
-        // You'll also want to load the data for the new week here
-    };
+  const handleWeekChange = (weekStart) => {
+    setCurrentWeekStart(weekStart);
+    // If there's no data for the new week, set it to default data.
+    setDataByWeek(prevData => ({
+      ...prevData,
+      [weekStart]: prevData[weekStart] || new Array(labels.length).fill(0),
+    }));
+  };
 
-    return (
-        <View style={{ alignItems: 'center' }}>
-            <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 5 }}>
-                <TouchableOpacity onPress={handlePreviousWeek}>
-                    <Text>{"<"}</Text>
-                </TouchableOpacity>
-                <Text style={{ flex: 1, textAlign: 'center' }}>
-                    {formatDateRange(currentWeekStart)}
-                </Text>
-                <TouchableOpacity onPress={handleNextWeek}>
-                    <Text>{">"}</Text>
-                </TouchableOpacity>
-            </View>
-            <TouchableOpacity onPress={onButtonPress} style={{ alignItems: 'center' }}>
-                <LineChart
-                    data={{
-                        labels: labels,
-                        datasets: [{ data: data }],
-                    }}
-                    width={300}
-                    height={200}
-                    chartConfig={{
-                        backgroundGradientFrom: '#fff',
-                        backgroundGradientTo: '#fff',
-                        color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
-                    }}
-                />
-            </TouchableOpacity>
-            {/* Graph Title */}
-            <Text style={{ marginTop: 8, fontSize: 16, fontWeight: 'bold' }}>
-                {trackerTitle}
-            </Text>
-        </View>
-    );
+  const handlePreviousWeek = () => {
+    const previousWeekStart = moment(currentWeekStart).subtract(1, 'week').format('YYYY-MM-DD');
+    handleWeekChange(previousWeekStart);
+  };
+
+  const handleNextWeek = () => {
+    const nextWeekStart = moment(currentWeekStart).add(1, 'week').format('YYYY-MM-DD');
+    handleWeekChange(nextWeekStart);
+  };
+
+  // Retrieves the data for the current week or defaults to zeroes if none exist.
+  const currentData = dataByWeek[currentWeekStart] || new Array(labels.length).fill(0);
+
+  return (
+    <View style={{ alignItems: 'center' }}>
+      <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 5 }}>
+        <TouchableOpacity onPress={handlePreviousWeek}>
+          <Text>{"<"}</Text>
+        </TouchableOpacity>
+        <Text style={{ flex: 1, textAlign: 'center' }}>
+          {formatDateRange(currentWeekStart)}
+        </Text>
+        <TouchableOpacity onPress={handleNextWeek}>
+          <Text>{">"}</Text>
+        </TouchableOpacity>
+      </View>
+      <TouchableOpacity onPress={onButtonPress} style={{ alignItems: 'center' }}>
+        <LineChart
+          data={{
+            labels: labels,
+            datasets: [{ data: currentData }],
+          }}
+          width={300}
+          height={200}
+          chartConfig={{
+            backgroundGradientFrom: '#fff',
+            backgroundGradientTo: '#fff',
+            color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+          }}
+        />
+      </TouchableOpacity>
+      {/* Graph Title */}
+      <Text style={{ marginTop: 8, fontSize: 16, fontWeight: 'bold' }}>
+        {trackerTitle}
+      </Text>
+    </View>
+  );
 };
 
 export default GraphWithButton;
